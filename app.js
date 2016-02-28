cde7f96330efc57a8158be39a2bdec3ebe07df31
robsongomes <nosbor84@gmail.com>
2016-02-28 20:16:22 +0000

    DELETE and PATCH added

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require('express'),
-    mongoose = require('mongoose'),
-    bodyParser = require('body-parser');
-    
-var mongo_db = process.env.MONGO_URL || 'mongodb://localhost/test';     
+  mongoose = require('mongoose'),
+  bodyParser = require('body-parser');
 
-var db = mongoose.connect(mongo_db);
+var mongo_db = process.env.MONGO_URL || 'mongodb://localhost/test';
+
+mongoose.connect(mongo_db);
+
 var Zip = require('./Zip');
 
 var app = express();
@@ -12,18 +13,21 @@ var app = express();
 var port = process.env.PORT || 3000;
 var ip = process.env.IP || 'localhost';
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
 
 var router = express.Router();
 
-router.route('/zips')
+router.route('/')
   .post((req, res) => {
     var zip = new Zip(req.body);
     zip.save((err) => {
       if (err) {
         res.status(500).send(err);
-      } else {
+      }
+      else {
         res.status(201).send(zip);
       }
     });
@@ -40,28 +44,79 @@ router.route('/zips')
     Zip.find(query, (err, zips) => {
       if (err) {
         res.status(500).send(err);
-      } else {
+      }
+      else {
         res.json(zips);
       }
     }).limit(5);
   });
 
-router.route('/zips/:zipId')
-  .get((req, res) => {
-    Zip.findOne({"_id" : req.params.zipId}, (err, zip) => {
-      if (err) {
-        res.status(500).send(err);
-      } else {
-        res.json(zip);
-      }
-    });
+router.use('/:zipId', (req, res, next) => {
+  Zip.findById(req.params.zipId, (err, zip) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    else if (zip) {
+      req.zip = zip;
+      next();
+    }
+    else {
+      res.status(404).send('No zip found');
+    }
   });
-
-app.get('/', (req,res) => {
-  res.send('Welcome to Zip API');
 });
 
-app.use('/api', router);
+router.route('/:zipId')
+  .delete((req, res) => {
+    req.zip.remove((err) => {
+      if (err) {
+        res.status(500).send(err);
+      }
+      else {
+        res.status(204).send('Removed');
+      }
+    });
+  })
+  .put((req, res) => {
+    req.zip.city = req.body.city;
+    req.zip.pop = req.body.pop;
+    req.zip.state = req.body.state;
+    req.zip.loc = req.body.loc;
+    req.zip.save((err) => {
+      if (err) {
+        res.status(500).send(err);
+      }
+      else {
+        res.status(201).send(req.zip);
+      }
+    });
+  })
+  .patch((req, res) => {
+    if (req.body._id)
+      delete req.body._id;
+
+    for (var p in req.body) {
+      req.zip[p] = req.body[p];
+    }
+
+    req.zip.save((err) => {
+      if (err) {
+        res.status(500).send(err);
+      }
+      else {
+        res.status(201).send(req.zip);
+      }
+    });
+  })
+  .get((req, res) => {
+    res.json(req.zip);
+  });
+
+app.use('/api/zips', router);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Zip API');
+});
 
 app.listen(port, ip, () => {
   console.log('Running on ' + ip + ":" + port);
