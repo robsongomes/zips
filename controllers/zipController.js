96cfec90f2c1409f0bec2cd20eaaea40ac1d6107
robsongomes <nosbor84@gmail.com>
2016-03-04 00:38:42 +0000

    Refactoring of controller. Code moved from router to controller

--- controllers/zipController.js
+++ controllers/zipController.js
@@ -10,7 +10,7 @@ var zipController = (Zip) => {
                 res.status(201).send(zip);
             }
         });
-    }
+    };
     
     var get = (req, res) => {
         var query = {};
@@ -29,11 +29,79 @@ var zipController = (Zip) => {
                 res.json(zips);
             }
         }).limit(5);
-    }
+    };
+    
+    var getById = (req, res) => {
+        res.json(req.zip);
+    };
+    
+    var del = (req, res) => {
+        req.zip.remove((err) => {
+            if (err) {
+                res.status(500).send(err);
+            }
+            else {
+                res.status(204).send('Removed');
+            }
+        });
+    };
+    
+    var put = (req, res) => {
+        req.zip.city = req.body.city;
+        req.zip.pop = req.body.pop;
+        req.zip.state = req.body.state;
+        req.zip.loc = req.body.loc;
+        req.zip.save((err) => {
+            if (err) {
+                res.status(500).send(err);
+            }
+            else {
+                res.status(201).send(req.zip);
+            }
+        });
+    };
+    
+    var patch = (req, res) => {
+        if (req.body._id)
+            delete req.body._id;
+
+        for (var p in req.body) {
+            req.zip[p] = req.body[p];
+        }
+
+        req.zip.save((err) => {
+            if (err) {
+                res.status(500).send(err);
+            }
+            else {
+                res.status(201).send(req.zip);
+            }
+        });
+    };
+    
+    var findById = (req, res, next) => {
+        Zip.findById(req.params.zipId, (err, zip) => {
+            if (err) {
+                res.status(500).send(err);
+            }
+            else if (zip) {
+                req.zip = zip;
+                next();
+            }
+            else {
+                res.status(404).send('No zip found');
+            }
+        });
+    };
     
     return {
         post : post,
-        get  : get
+        get  : get,
+        del  : del,
+        put  : put,
+        patch : patch,
+        getById : getById,
+        findById : findById
     }
 }
 
